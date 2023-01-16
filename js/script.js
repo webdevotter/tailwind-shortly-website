b80a756e49c1bd15fc30514a6bd304d5b3e647ab
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2023-01-16 10:40:11 -0600

    Mobile Menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
 
+btn.addEventListener('click', navToggle);
 linkForm.addEventListener('submit', formSubmit);
 
-function formSubmit(e) {
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
 
+function formSubmit(e) {
+  // URL Validation
   function validURL(str) {
     var pattern = new RegExp(
       '^(https?:\\/\\/)?' + // protocol
